117ee0f5807597b6d6da0821c4906d287b7e929d
Melborpco <accounts@melborp.co>
2024-04-25 09:55:46 -0500

    Update slide IDs in slide_3.html and slide_4.html

--- src/js/app.js
+++ src/js/app.js
@@ -354,20 +354,20 @@ function startChat() {
 }
 
 if (document.getElementById("as__ciber_t1_slider")) {
-  window.addEventListener("load", () => {
-    // window.location.hash = "";
-  });
+  // window.addEventListener("load", () => {
+  //   window.location.hash = "";
+  // });
   document.querySelectorAll(".btn").forEach((btn) => {
     btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      let hash = e.target.getAttribute("href");
-      if (hash == "#") return;
-      if (hash == null) hash = "#slide2";
-      window.location.hash = hash;
-
-      lenis.scrollTo("#as__ciber_t1_slider", {
-        offset: -60,
-      });
+      e.stopPropagation();
+      // e.preventDefault();
+      // let hash = e.target.getAttribute("href");
+      // if (hash == "#") return;
+      // if (hash == null) hash = "#slide2";
+      // window.location.hash = hash;
+      // lenis.scrollTo("#as__ciber_t1_slider", {
+      //   offset: -60,
+      // });
     });
   });
 }
